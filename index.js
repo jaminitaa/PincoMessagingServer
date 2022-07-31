4c328a41d7b96b15ccd5c7f0b152f98e9c5a77bb
Iris Chen <105895211+jaminitaa@users.noreply.github.com>
2022-07-31 03:02:14 +0000

    update credential location

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const fs = require('fs');
 
 const { initializeApp: initializeAdminApp, applicationDefault } = require('firebase-admin/app');
 const { getMessaging } = require('firebase-admin/messaging');
 
 const { initializeApp } = require('firebase/app');
-const { getFirestore, collection, query, where, onSnapshot } = require("firebase/firestore");
-
+const { getFirestore, collection, query, where, onSnapshot, getDocs } = require("firebase/firestore");
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS = '/home/runner/PincoMessagingServer-1/secret-key.json';
 
@@ -30,32 +28,64 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const q = query(collection(db, "userStatus"), where("sosEvent", "==", true));
-const unsubscribe = onSnapshot(q, (querySnapshot) => {
+// get a array of checkInOnTime == false and active == true
+const q = query(collection(db, "checkInSession"), where("checkInOnTime", "==", false), where("active", "==", true));
+
+const unsubscribe = onSnapshot(q, async(querySnapshot) => {
+  const notCheckedInSessions = [];
+  querySnapshot.forEach((doc) => {
+      notCheckedInSessions.push(doc.data());
+  });
+  console.log(notCheckedInSessions.length + " users not checked in");
+  const notCheckedInUsers = await getNotCheckedInUsers(notCheckedInSessions);
+  // put into sendCloudMessageToObservers function as sosUsers parameter
+  console.log(notCheckedInUsers);
+  sendCloudMessageToObservers(notCheckedInUsers, "Your friend Brenda has missed the check-in alarm! We suggest you check with Brenda as soon as possible. Click to check this out");
+  
+});
+
+const q2 = query(collection(db, "userStatus"), where("sosEvent", "==", true));
+const unsubscribe2 = onSnapshot(q2, (querySnapshot) => {
   const sosUsers = [];
   querySnapshot.forEach((doc) => {
       sosUsers.push(doc.data());
-      // console.log(doc.data());
+    // { email:..., lat:... long:... observertokens: ... }
   });
-  sendCloudMessageToObservers(sosUsers);
+  sendCloudMessageToObservers(sosUsers, "Your friend Brenda need your help! Click to check this out");
 });
 
 
-function sendCloudMessageToObservers (sosUsers) {
+async function getNotCheckedInUsers (notCheckedInSessions) {
+  // foreach document, find in userStatus where checkinsession.userid == userstatus.userid
+  const notCheckedInUsers = [];
+  for (const session of notCheckedInSessions) {
+    const userStatusQ = query(collection(db, "userStatus"), where("userID", "==", session.userID));
+    const userStatusQSnapshot = await getDocs(userStatusQ);
+    userStatusQSnapshot.forEach((userStatusDoc) => {
+      notCheckedInUsers.push(userStatusDoc.data());
+    });
+  }
+  // return a array of userStatus documents/objects
+  return notCheckedInUsers;
+}
+
+function sendCloudMessageToObservers (sosUsers, text) {
     sosUsers.forEach((user) => {
         if (user.observerTokens && user.observerTokens.length) {
             const message = {
               // TODO: send userID in data object
-                data: {text: "Your friend is in danger! Check this out"},
+                data: {text},
                 tokens: user.observerTokens,
             };
             getMessaging().sendMulticast(message)
               .then((response) => {
-                console.log(response.successCount + ' messages were sent successfully');
+                console.log(response.successCount + ' messages were sent successfully' + ' text: ' + text);
             });
         }
     })
 }
+
+
 // Create a list containing up to 500 registration tokens.
 // These registration tokens come from the client FCM SDKs.
 // const registrationTokens = [
@@ -64,7 +94,7 @@ function sendCloudMessageToObservers (sosUsers) {
 
 
 
-// replit requires something to be served to '/' to keep alive
+
 const expressApp = express();
 
 expressApp.get('/', (req, res) => {
